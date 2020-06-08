dbf441b8f0f1dc223cbf62a146e8efa7ed884740
David Foster <dlfosterii@yahoo.com>
2020-06-08 12:10:03 -0400

    first card added

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+
+class Card extends Component {
+  render() {
+      
+      return (
+          <div className="Card">
+             {this.props.title}
+             <br></br>
+             {this.props.likes >= 0 ? (
+                 <sup>likes: { this.props.likes }</sup>
+             )
+             : ''}
+          </div>
+      )
+  }
+}
+
+export default Card;

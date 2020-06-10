a22a616532fbef14aaf8167f2cc3d148485f585e
David Foster <dlfosterii@yahoo.com>
2020-06-10 16:17:17 -0400

    Step 2 complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import Card from './components/Card';
 import MemoryCard from './components/MemoryCard'
 
@@ -9,10 +9,10 @@ import { render } from '@testing-library/react';
 function generateDeck() {
   const symbols = ['∆', 'ß', '£', '§', '•', '$', '+', 'ø'];
   let deck = [];
-  for (let i = 0 ; i < 16; i++) {
+  for (let i = 0; i < 16; i++) {
     deck.push({
       isFlipped: false,
-      symbol: symbols[i%8],
+      symbol: symbols[i % 8],
     }
     )
   }
@@ -25,8 +25,8 @@ function generateDeck() {
  */
 function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
 }
@@ -37,47 +37,41 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      deck: [generateDeck()],
-      pickedCards: [],    
+      deck: generateDeck(),
+      pickedCards: [],
     }
   }
 
 
   render() {
+    let cardsJSX = this.state.deck.map((card, index) => {
+      return <MemoryCard />
+    });
+
     return (
       <div className="App">
         <header className="App-header">
-        <h1>Memory Game</h1>
-        <h4 className="subHeader" >Match cards to win</h4>
+          <h1>Memory Game</h1>
+          <h4 className="subHeader" >Match cards to win</h4>
         </header>
-      <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-      <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-      <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-      <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-  
+        <div>
+          {cardsJSX.slice(0, 4)}
+        </div>
+        <div>
+
+          {cardsJSX.slice(4, 8)}
+        </div>
+        <div>
+
+          {cardsJSX.slice(8, 12)}
+
+        </div>
+        <div>
+          {cardsJSX.slice(12, 16)}
+        </div>
       </div>
     );
-  
+
   }
 }
 
